63333d464bc541a3a4a528ff53680a341eeea40f
chenmal <chenmalish@gmail.com>
2020-07-14 17:23:50 +0300

    Updates

---
+++ precache-manifest.8a386b58a8a98df9cce844b5e439ee7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71108cf30e2cf8353ff4b58037e0c5f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc0a06781ad0f7c0a389",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fb6a50d314fe59b01cbe",
+    "url": "/static/js/2.f2d3eb78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f2d3eb78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc0a06781ad0f7c0a389",
+    "url": "/static/js/main.f3483cd6.chunk.js"
+  },
+  {
+    "revision": "a84d059159d9808de5e2",
+    "url": "/static/js/runtime-main.1133875e.js"
+  }
+]);
